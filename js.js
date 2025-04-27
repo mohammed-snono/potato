15db2e02f792dd9dab8dcec900cbadf0c649dd47
mohammed-snono <m.snono87@gmail.com>
2025-04-27 22:52:16 +0300

    whatever

---
+++ js.js
@@ -0,0 +1,35 @@
+
+// let butt=document.getElementById('b1')
+// butt.style.color='red'
+
+// function ck1(){
+//     document.getElementById('h1').innerText="wellcome"
+// }
+
+
+
+// let b2=document.getElementById('b2')
+// let x=document.getElementById('h1')
+
+// b2.addEventListener("click",function(){
+//     x.innerText=("hellow world")
+// })
+
+
+// function x(){
+//     if(true){
+//         var a=10
+//     }
+
+// console.log(a)
+// }
+
+// x()
+
+const name= "mohammed"
+const age=20
+
+export {
+    name,
+    age
+}
---
+++ new.js
@@ -0,0 +1,3 @@
+import{name,age} from "./js.js"
+console.log(name)
+console.log(age)
